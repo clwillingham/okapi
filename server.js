f2e742be027dbdc798665dbf6f7de14d3481e3d6
Chris <clwillingham@gmail.com>
2014-01-15 20:23:10 -0500

    adding basic MVC stuff

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
  * Created by Chris Willingham on 1/15/14.
  */
 var express = require("express"),
+    fs      = require('fs'),
+    path    = require('path'),
     app     = express(),
     port    = parseInt(process.env.PORT, 10) || 3000;
 
@@ -15,5 +17,21 @@ app.configure(function(){
   }));
   app.use(app.router);
 });
+
+var controllerFiles = fs.readdirSync('./controllers');
+var modelFiles = fs.readdirSync('./models');
+
+for(var i in controllerFiles){
+  var controller = require('./controllers/' + controllerFiles[i]);
+  var routes = controller.routes;
+  for(var route in routes){
+    var action = routes[route];
+    var routeData = route.split(' ');
+    var method = routeData[0];
+    var url = path.join(controller.root || '/', routeData[1] || '');
+    app[method](url, action);
+    console.log(method + ': ' + url);
+  }
+}
 
 app.listen(port);
